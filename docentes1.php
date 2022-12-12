2eb3872920567e8d007a05323e6020560af80d76
michaelq2020 <michaelquispechavez@gmail.com>
2022-12-12 15:40:55 -0500

    conexio
    conexiones y gentella

--- docentes1.php
+++ docentes1.php
@@ -113,3 +113,119 @@ include "include/verificar_sesion.php";
                         </label>
                         <div class="col-md-6 col-sm-6 col-xs-12">
                         <select name="programa_estudio" id="programa_estudio" class="form-control col-md-7 col-xs-12">
+                        <option value="">Seleccione</option>
+                          <?php
+                          $buscar_pe = buscarProgramaEstudio($conexion);
+                          while ($res_b_pe = mysqli_fetch_array($buscar_pe)) {
+                          ?>
+                          <option value="<?php echo $res_b_pe['id']; ?>"><?php echo $res_b_pe['nombre']; ?></option>
+                          <?php
+                          };
+                          ?>
+                        </select>
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">cond_laboral:
+                        </label>
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                        <select name="semestre" id="semestre" class="form-control col-md-7 col-xs-12">
+                          <option value="">Seleccione</option>
+                          <?php
+                          $buscar_sem = buscarSemestre($conexion);
+                          while ($res_b_sem = mysqli_fetch_array($buscar_sem)) {
+                          ?>
+                          <option value="<?php echo $res_b_sem['id']; ?>"><?php echo $res_b_sem['descripcion']; ?></option>
+                          <?php
+                          };
+                          ?>
+                        </select>
+                        </div>
+                      </div>
+                      
+                      <div class="form-group">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">id_cargo :
+                        </label>
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                        <select name="condicion" id="condicion" class="form-control col-md-7 col-xs-12">
+                          <option value="">Seleccione</option>
+                          <?php
+                          $buscar_condicion = buscarCondicion($conexion);
+                          while ($res_b_condicion = mysqli_fetch_array($buscar_condicion)) {
+                          ?>
+                          <option value="<?php echo $res_b_condicion['id']; ?>"><?php echo $res_b_condicion['descripcion']; ?></option>
+                          <?php
+                          };
+                          ?>
+                        </select>
+                        </div>
+                      </div>
+                      
+
+                      <div class="ln_solid"></div>
+                      <div class="form-group">
+                        <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+                          <button class="btn btn-primary" type="button">Cancelar</button>
+						  <button class="btn btn-primary" type="reset">Limpiar</button>
+                          <button type="submit" class="btn btn-success">Guardar</button>
+                        </div>
+                      </div>
+
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
+        <!-- /page content -->
+        <!-- footer content -->
+        <footer>
+          <div class="pull-right">
+            Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
+          </div>
+          <div class="clearfix"></div>
+        </footer>
+        <!-- /footer content -->
+      </div>
+    </div>
+    <!-- jQuery -->
+    <script src="Gentella/vendors/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap -->
+    <script src="Gentella/vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+    <!-- FastClick -->
+    <script src="Gentella/vendors/fastclick/lib/fastclick.js"></script>
+    <!-- NProgress -->
+    <script src="Gentella/vendors/nprogress/nprogress.js"></script>
+    <!-- Chart.js -->
+    <script src="Gentella/vendors/Chart.js/dist/Chart.min.js"></script>
+    <!-- gauge.js -->
+    <script src="Gentella/vendors/gauge.js/dist/gauge.min.js"></script>
+    <!-- bootstrap-progressbar -->
+    <script src="Gentella/vendors/bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
+    <!-- iCheck -->
+    <script src="Gentella/vendors/iCheck/icheck.min.js"></script>
+    <!-- Skycons -->
+    <script src="Gentella/vendors/skycons/skycons.js"></script>
+    <!-- Flot -->
+    <script src="Gentella/vendors/Flot/jquery.flot.js"></script>
+    <script src="Gentella/vendors/Flot/jquery.flot.pie.js"></script>
+    <script src="Gentella/vendors/Flot/jquery.flot.time.js"></script>
+    <script src="Gentella/vendors/Flot/jquery.flot.stack.js"></script>
+    <script src="Gentella/vendors/Flot/jquery.flot.resize.js"></script>
+    <!-- Flot plugins -->
+    <script src="Gentella/vendors/flot.orderbars/js/jquery.flot.orderBars.js"></script>
+    <script src="Gentella/vendors/flot-spline/js/jquery.flot.spline.min.js"></script>
+    <script src="Gentella/vendors/flot.curvedlines/curvedLines.js"></script>
+    <!-- DateJS -->
+    <script src="Gentella/vendors/DateJS/build/date.js"></script>
+    <!-- JQVMap -->
+    <script src="Gentella/vendors/jqvmap/dist/jquery.vmap.js"></script>
+    <script src="Gentella/vendors/jqvmap/dist/maps/jquery.vmap.world.js"></script>
+    <script src="Gentella/vendors/jqvmap/examples/js/jquery.vmap.sampledata.js"></script>
+    <!-- bootstrap-daterangepicker -->
+    <script src="Gentella/vendors/moment/min/moment.min.js"></script>
+    <script src="Gentella/vendors/bootstrap-daterangepicker/daterangepicker.js"></script>
+    <!-- Custom Theme Scripts -->
+    <script src="Gentella/build/js/custom.min.js"></script>
+  </body>
+</html>
