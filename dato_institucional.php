25c7810e2b6eee1eb6a4d6ab837edd0b234af486
michaelq2020 <michaelquispechavez@gmail.com>
2022-12-12 15:30:16 -0500

    encabezado
    encabezados

--- dato_institucional.php
+++ dato_institucional.php
@@ -34,3 +34,37 @@ include "include/verificar_sesion.php";
     <div class="container body">
       <div class="main_container">
       <?php include "include/menu.php" ?> 
+      <!-- Menu en la parte superior -->
+        <!-- page content -->
+        <div class="right_col" role="main">
+        <div class="row">
+              <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="x_panel">
+                  <div class="x_title">
+                    <h2>Relacion de Estudiantes</h2>
+                    <ul class="nav navbar-right">
+                      <li>
+                        <a href="estudiante.php" class="btn btn-success">Agregar Nuevo</a>
+                      </li>
+                    </ul>
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content">
+                    
+                    <table id="example" class="table table-striped table-bordered">
+                      <thead>
+                        <tr>
+                          <th>Id</th>
+                          <th>RUC</th>
+                          <th>NOMBRE INSTITUCIONAL</th>
+                          <th>DEPARTAMENTO</th>
+                          <th>PROVINCIA</th>
+                          <th>DISTRITO</th>
+                          <th>DIRECCION</th>
+                          <th>TELEFONO</th>
+                          <th>CORREO</th>
+                          <th>NRO RESOLUCION</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                          
