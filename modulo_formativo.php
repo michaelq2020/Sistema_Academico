795886915b03182e002b9284df1ab566f7afb67b
michaelq2020 <michaelquispechavez@gmail.com>
2022-12-12 08:45:25 -0500

    Update modulo_formativo.php
    el proceso, buscar

--- modulo_formativo.php
+++ modulo_formativo.php
@@ -133,7 +133,21 @@ include "include/verificar_sesion.php";
     $(document).ready(function() {
     $('#example').DataTable({
       "language":{
-  
+    "processing": "Procesando...",
+    "lengthMenu": "Mostrar _MENU_ registros",
+    "zeroRecords": "No se encontraron resultados",
+    "emptyTable": "Ningún dato disponible en esta tabla",
+    "sInfo": "Mostrando del _START_ al _END_ de un total de _TOTAL_ registros",
+    "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+    "infoFiltered": "(filtrado de un total de _MAX_ registros)",
+    "search": "Buscar:",
+    "infoThousands": ",",
+    "loadingRecords": "Cargando...",
+    "paginate": {
+        "first": "Primero",
+        "last": "Último",
+        "next": "Siguiente",
+        "previous": "Anterior"
     },
       }
     });
