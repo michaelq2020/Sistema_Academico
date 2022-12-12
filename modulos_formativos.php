1520d3ad6e265863d5c83778bb7479f76241b170
michaelq2020 <michaelquispechavez@gmail.com>
2022-12-12 08:59:37 -0500

    Update modulos_formativos.php
    parte en el cual se va modificar

--- modulos_formativos.php
+++ modulos_formativos.php
@@ -29,3 +29,127 @@ include "include/verificar_sesion.php";
     <!-- Custom Theme Style -->
     <link href="Gentella/build/css/custom.min.css" rel="stylesheet">
   </head>
+  body class="nav-md">
+    <div class="container body">
+      <div class="main_container">
+        <?php include "include/menu.php" ?>  
+        <!-- Menu en la parte superior -->
+        <!-- page content -->
+        <div class="right_col" role="main">
+        <div class="row">
+              <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="x_panel">
+                  <div class="x_title">
+                    <h2>Registro de Modulos Formativos</h2>
+                    
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content">
+                    <br />
+                    <form class="form-horizontal form-label-left" method="POST" action="operaciones/registrar_modulo.php">
+
+                      <div class="form-group">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">id :
+                        </label>
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                          <input type="number" name="id" maxlength="8" required="required" class="form-control col-md-7 col-xs-12">
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Descripcion :
+                        </label>
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                          <input type="text" name="descripcion" maxlength="9" required="required" class="form-control col-md-7 col-xs-12">
+                        </div>
+                      </div>
+                      <div class="form-group">
+                      <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Numero Modular :
+                        </label>
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                          <input type="number" name="nro_modulo" maxlength="9" required="required" class="form-control col-md-7 col-xs-12">
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Programa de Estudios :
+                        </label>
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                        <select name="programa_estudio" id="programa_estudio" class="form-control col-md-7 col-xs-12">
+                          <option value="">Seleccione</option>
+                          <?php
+                          $buscar_pe = buscarProgramaEstudio($conexion);
+                          while ($res_b_pe = mysqli_fetch_array($buscar_pe)) {
+                          ?>
+                          <option value="<?php echo $res_b_pe['id']; ?>"><?php echo $res_b_pe['nombre']; ?></option>
+                          <?php
+                          };
+                          ?>
+                        </select>
+                        </div>
+                      </div>
+                      <div class="ln_solid"></div>
+                      <div class="form-group">
+                        <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+                          <button class="btn btn-primary" type="button">Cancelar</button>
+						              <button class="btn btn-primary" type="reset">Limpiar</button>
+                          <button type="submit" class="btn btn-success">Guardar</button>
+                        </div>
+                      </div>
+
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
+        <!-- /page content -->
+        <!-- footer content -->
+        <footer>
+          <div class="pull-right">
+            Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
+          </div>
+          <div class="clearfix"></div>
+        </footer>
+        <!-- /footer content -->
+      </div>
+    </div>
+    <!-- jQuery -->
+    <script src="Gentella/vendors/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap -->
+    <script src="Gentella/vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+    <!-- FastClick -->
+    <script src="Gentella/vendors/fastclick/lib/fastclick.js"></script>
+    <!-- NProgress -->
+    <script src="Gentella/vendors/nprogress/nprogress.js"></script>
+    <!-- Chart.js -->
+    <script src="Gentella/vendors/Chart.js/dist/Chart.min.js"></script>
+    <!-- gauge.js -->
+    <script src="Gentella/vendors/gauge.js/dist/gauge.min.js"></script>
+    <!-- bootstrap-progressbar -->
+    <script src="Gentella/vendors/bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
+    <!-- iCheck -->
+    <script src="Gentella/vendors/iCheck/icheck.min.js"></script>
+    <!-- Skycons -->
+    <script src="Gentella/vendors/skycons/skycons.js"></script>
+    <!-- Flot -->
+    <script src="Gentella/vendors/Flot/jquery.flot.js"></script>
+    <script src="Gentella/vendors/Flot/jquery.flot.pie.js"></script>
+    <script src="Gentella/vendors/Flot/jquery.flot.time.js"></script>
+    <script src="Gentella/vendors/Flot/jquery.flot.stack.js"></script>
+    <script src="Gentella/vendors/Flot/jquery.flot.resize.js"></script>
+    <!-- Flot plugins -->
+    <script src="Gentella/vendors/flot.orderbars/js/jquery.flot.orderBars.js"></script>
+    <script src="Gentella/vendors/flot-spline/js/jquery.flot.spline.min.js"></script>
+    <script src="Gentella/vendors/flot.curvedlines/curvedLines.js"></script>
+    <!-- DateJS -->
+    <script src="Gentella/vendors/DateJS/build/date.js"></script>
+    <!-- JQVMap -->
+    <script src="Gentella/vendors/jqvmap/dist/jquery.vmap.js"></script>
+    <script src="Gentella/vendors/jqvmap/dist/maps/jquery.vmap.world.js"></script>
+    <script src="Gentella/vendors/jqvmap/examples/js/jquery.vmap.sampledata.js"></script>
+    <!-- bootstrap-daterangepicker -->
+    <script src="Gentella/vendors/moment/min/moment.min.js"></script>
+    <script src="Gentella/vendors/bootstrap-daterangepicker/daterangepicker.js"></script>
+    <!-- Custom Theme Scripts -->
+    <script src="Gentella/build/js/custom.min.js"></script>
+  </body>
+</html>
